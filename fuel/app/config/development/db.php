8f853c3186431bef8c1186ed3cb9e0c6df1896fa
Hitoshi ASANO <orcaaoshi@gmail.com>
2013-04-13 16:33:37 +0900

    DSNを修正

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;port=8889;dbname=fuelhandson',
+			'dsn'        => 'mysql:host=127.0.0.1;port=8889;dbname=fuelhandson',
 			'username'   => 'fuelhandson',
 			'password'   => '',
 		),
