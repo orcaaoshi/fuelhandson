f78c6c487d898bd46da8a3509725c96d22b0b0e9
Hitoshi ASANO <orcaaoshi@gmail.com>
2013-04-13 15:30:57 +0900

    Change config/development/db.php

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
-			'username'   => 'root',
-			'password'   => 'root',
+			'dsn'        => 'mysql:host=localhost;port=8889;dbname=fuelhandson',
+			'username'   => 'fuelhandson',
+			'password'   => '',
 		),
 	),
 );
