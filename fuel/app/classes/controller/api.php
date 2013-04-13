1458b6943b7d6570d0fbaf7e25f82009dfcc7d2e
Hitoshi ASANO <orcaaoshi@gmail.com>
2013-04-13 16:34:51 +0900

    Controller_Restを継承したAPIを作成

---
+++ fuel/app/classes/controller/api.php
@@ -0,0 +1,39 @@
+<?php
+
+class Controller_Api extends Controller_Rest
+{
+	public function get_list()
+	{
+		return \Model_Bb::find('all');
+	}
+
+	//public function post_create()
+	public function get_create()
+	{
+		$bb = \Model_Bb::forge();
+		$bb->post_date = \Input::get('post_date');
+		$bb->message = \Input::get('message');
+		$bb->save();
+
+		return array(
+			'id' => $bb->id,
+			'post_date' => $bb->post_date,
+			'message' => $bb->message,
+		);
+	}
+
+	//public function post_edit()
+	public function get_edit()
+	{
+		$bb = \Model_Bb::find(\Input::get('id'));
+		$bb->post_date = \Input::get('post_date');
+		$bb->message = \Input::get('message');
+		$bb->save();
+
+		return array(
+			'id' => $bb->id,
+			'post_date' => $bb->post_date,
+			'message' => $bb->message,
+		);
+	}
+}
